f44a133fdb6fa1b44d70c725ec4db4c7b3055e1a
Jonathan <jonsasser@gmail.com>
2025-01-12 17:32:35 -0800

    Split goal category and subcategory seed functions from survey seed functions

--- database/seed/survey_seed.py
+++ database/seed/survey_seed.py
@@ -19,6 +19,8 @@ def user_survey_seed():
         return redirect("/")
 
     # REVISIT VALIDATION FOR ENTIRETY OF SEEDING FUNCTIONS
+    goal_category_seed()
+    goal_subcategory_seed()
     survey_category_seed()
     survey_topic_seed()
     prepared_question_data = prepare_survey_question_data()
@@ -27,6 +29,75 @@ def user_survey_seed():
     survey_generic_answer_seed()
     process_survey_question_and_map_data(prepared_question_data)
     # survey_answer_seed and survey_example_seed in prepare_survey_question_data
+
+    return
+
+
+# TODO: survey and goal categories, subcategories and topics will be merged.  Temporary independent seed functions until then...
+def goal_category_seed():
+    query = """
+    INSERT INTO categories (category_slug, name, created_at, updated_at)
+    VALUES
+      ('foundations', 'Foundations', NOW(), NOW()),
+      ('your-why', 'Defining Your Why', NOW(), NOW()),
+      ('health-wellness', 'Health and Wellness', NOW(), NOW()),
+      ('social-community', 'Social and Community Engagement', NOW(), NOW()),
+      ('recreation-travel', 'Recreation and Travel', NOW(), NOW()),
+      ('spirituality-life-purpose', 'Spirituality and Life Purpose', NOW(), NOW()),
+      ('career-professional-development', 'Career and Professional Development', NOW(), NOW()),
+      ('creative-expression-hobbies', 'Creative Expression and Hobbies', NOW(), NOW()),
+      ('wealth-finance', 'Wealth Building and Financial Health', NOW(), NOW()),
+      ('environment-success', 'Environment and Success', NOW(), NOW())
+    ON DUPLICATE KEY UPDATE updated_at = NOW();
+      """
+    UserSurvey.db.query_db(query)
+
+    return
+
+
+def goal_subcategory_seed():
+    batch_data = {}
+
+    # Organize topics into batches by category (key)
+    for category_slug, topics in survey_topic_data.items():
+        for topic in topics:
+            if category_slug not in batch_data:
+                batch_data[category_slug] = []
+            batch_data[category_slug].append(
+                {"topic_slug": topic["topic_slug"], "topic_name": topic["name"]}
+            )
+
+    # Retrieve the mapping of category slugs to category IDs
+    survey_category_ids = (
+        get_all_survey_category_ids()
+    )  # Should return a dictionary like {'health-wellness': 1, 'social-community': 2, ...}
+
+    # Prepare and execute batch insert queries for each category
+    for category_slug, topic_batch in batch_data.items():
+        # Check if the category slug exists in the retrieved category IDs
+        if category_slug in survey_category_ids:
+            survey_category_id = survey_category_ids[category_slug]
+        else:
+            print(f"Category slug '{category_slug}' not found in the database.")
+            continue  # Skip this category if the ID is not found
+
+        # Create the base query
+        query = "INSERT INTO subcategories (category_id, subcategory_slug, name, created_at, updated_at) VALUES "
+        query_values = []
+
+        # Temporary secondary seed to subcategories, merge pending
+
+        # Add the topic values to the query
+        for topic in topic_batch:
+            values = f"({survey_category_id}, '{topic['topic_slug']}', '{topic['topic_name']}', NOW(), NOW())"
+            query_values.append(values)
+
+        # Combine the query and execute it
+        query += ", ".join(query_values)
+        query += " ON DUPLICATE KEY UPDATE updated_at = NOW();"
+
+        # Execute the query using your database method
+        UserSurvey.db.query_db(query)
 
     return
 
@@ -49,23 +120,7 @@ def survey_category_seed():
             """
     UserSurvey.db.query_db(query)
 
-    # Temporary secondary query to seed goal related categories, merge pending
-    query_2 = """
-        INSERT INTO categories (category_slug, name, created_at, updated_at)
-        VALUES
-            ('foundations', 'Foundations', NOW(), NOW()),
-            ('your-why', 'Defining Your Why', NOW(), NOW()),
-            ('health-wellness', 'Health and Wellness', NOW(), NOW()),
-            ('social-community', 'Social and Community Engagement', NOW(), NOW()),
-            ('recreation-travel', 'Recreation and Travel', NOW(), NOW()),
-            ('spirituality-life-purpose', 'Spirituality and Life Purpose', NOW(), NOW()),
-            ('career-professional-development', 'Career and Professional Development', NOW(), NOW()),
-            ('creative-expression-hobbies', 'Creative Expression and Hobbies', NOW(), NOW()),
-            ('wealth-finance', 'Wealth Building and Financial Health', NOW(), NOW()),
-            ('environment-success', 'Environment and Success', NOW(), NOW())
-        ON DUPLICATE KEY UPDATE updated_at = NOW();
-            """
-    UserSurvey.db.query_db(query_2)
+    return
 
 
 def survey_topic_seed():
@@ -100,32 +155,17 @@ def survey_topic_seed():
         query = "INSERT INTO survey_topics (survey_category_id, topic_slug, name, created_at, updated_at) VALUES "
         query_values = []
 
-        # Temporary secondary seed to subcategories, merge pending
-        query_2 = "INSERT INTO subcategories (category_id, subcategory_slug, name, created_at, updated_at) VALUES "
-        query_values_2 = []
-
         # Add the topic values to the query
         for topic in topic_batch:
             values = f"({survey_category_id}, '{topic['topic_slug']}', '{topic['topic_name']}', NOW(), NOW())"
             query_values.append(values)
 
-            # # Temporary secondary snippet, merge pending, OOPS, REDUNDANT?
-            # values_2 = f"({survey_category_id}, '{topic['topic_slug']}', '{topic['topic_name']}', NOW(), NOW())"
-            # query_values_2.append(values_2)
-
         # Combine the query and execute it
         query += ", ".join(query_values)
         query += " ON DUPLICATE KEY UPDATE updated_at = NOW();"
 
-        # TEMPORARY SECONDARY SNIPPET, MERGE PENDING
-        query_2 += ", ".join(query_values)
-        query_2 += " ON DUPLICATE KEY UPDATE updated_at = NOW();"
-
         # Execute the query using your database method
         UserSurvey.db.query_db(query)
-
-        # TEMPORARY SECONDARY QUERY, MERGE PENDING
-        UserSurvey.db.query_db(query_2)
 
     print(
         f"{sum(len(v) for v in survey_topic_data.values())} survey topics have been seeded."
