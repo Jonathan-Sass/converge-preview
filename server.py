be9640ac993a0c9c87931c26383315ed40bd9d16
Jonathan <jonsasser@gmail.com>
2025-01-16 17:44:17 -0800

    Added goals controller back to server.py

--- server.py
+++ server.py
@@ -4,6 +4,7 @@ from flask_app.controllers import user_surveys
 from flask_app.controllers import personal_routines
 from flask_app.controllers import routine_templates
 from flask_app.controllers import practices
+from flask_app.controllers import goals
 
 if __name__ == "__main__":
     app.run(debug=True)
