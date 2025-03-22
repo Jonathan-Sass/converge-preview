a18acac624ec3ce7fb037d51d6dd9823f25037b2
Jonathan <jonsasser@gmail.com>
2025-03-21 21:15:50 -0700

    Commenting deprecated methods - test before final deletion

--- database/seed/survey_seed.py
+++ database/seed/survey_seed.py
@@ -63,74 +63,75 @@ def test_seed():
     return
 
 
-# TODO: survey and goal categories, subcategories and subcategories will be merged.  Temporary independent seed functions until then...
-def goal_category_seed():
-    query = """
-    INSERT INTO categories (category_slug, name, created_at, updated_at)
-    VALUES
-      ('onboarding', 'Onboarding', NOW(), NOW()),
-      ('foundations', 'Foundations', NOW(), NOW()),
-      ('your-why', 'Defining Your Why', NOW(), NOW()),
-      ('health-wellness', 'Health and Wellness', NOW(), NOW()),
-      ('social-community', 'Social and Community Engagement', NOW(), NOW()),
-      ('recreation-travel', 'Recreation and Travel', NOW(), NOW()),
-      ('spirituality-life-purpose', 'Spirituality and Life Purpose', NOW(), NOW()),
-      ('career-professional-development', 'Career and Professional Development', NOW(), NOW()),
-      ('creative-expression-hobbies', 'Creative Expression and Hobbies', NOW(), NOW()),
-      ('wealth-finance', 'Wealth Building and Financial Health', NOW(), NOW()),
-      ('environment-success', 'Environment and Success', NOW(), NOW())
-    ON DUPLICATE KEY UPDATE updated_at = NOW();
-      """
-    UserSurvey.db.query_db(query)
+# DEPRECATED FOR UNIFIED CATEGORIES
+# # def goal_category_seed():
+#     query = """
+#     INSERT INTO categories (category_slug, name, created_at, updated_at)
+#     VALUES
+#       ('onboarding', 'Onboarding', NOW(), NOW()),
+#       ('foundations', 'Foundations', NOW(), NOW()),
+#       ('your-why', 'Defining Your Why', NOW(), NOW()),
+#       ('health-wellness', 'Health and Wellness', NOW(), NOW()),
+#       ('social-community', 'Social and Community Engagement', NOW(), NOW()),
+#       ('recreation-travel', 'Recreation and Travel', NOW(), NOW()),
+#       ('spirituality-life-purpose', 'Spirituality and Life Purpose', NOW(), NOW()),
+#       ('career-professional-development', 'Career and Professional Development', NOW(), NOW()),
+#       ('creative-expression-hobbies', 'Creative Expression and Hobbies', NOW(), NOW()),
+#       ('wealth-finance', 'Wealth Building and Financial Health', NOW(), NOW()),
+#       ('environment-success', 'Environment and Success', NOW(), NOW())
+#     ON DUPLICATE KEY UPDATE updated_at = NOW();
+#       """
+#     UserSurvey.db.query_db(query)
 
-    return
+#     return
 
 
-def goal_subcategory_seed():
-    batch_data = {}
+# DEPRECATED FOR UNIFIED SUBCATEGORIES
+# def goal_subcategory_seed():
+#     batch_data = {}
 
-    # Organize subcategories into batches by category (key)
-    for category_slug, subcategories in subcategory_data.items():
-        for subcategory in subcategories:
-            if category_slug not in batch_data:
-                batch_data[category_slug] = []
-            batch_data[category_slug].append(
-                {"subcategory_slug": subcategory["subcategory_slug"], "subcategory_name": subcategory["name"]}
-            )
+#     # Organize subcategories into batches by category (key)
+#     for category_slug, subcategories in subcategory_data.items():
+#         for subcategory in subcategories:
+#             if category_slug not in batch_data:
+#                 batch_data[category_slug] = []
+#             batch_data[category_slug].append(
+#                 {"subcategory_slug": subcategory["subcategory_slug"], "subcategory_name": subcategory["name"]}
+#             )
 
-    # Retrieve the mapping of category slugs to category IDs
-    category_ids = (
-        get_all_category_ids()
-    )  # Should return a dictionary like {'health-wellness': 1, 'social-community': 2, ...}
+#     # Retrieve the mapping of category slugs to category IDs
+#     category_ids = (
+#         get_all_category_ids()
+#     )  # Should return a dictionary like {'health-wellness': 1, 'social-community': 2, ...}
 
-    # Prepare and execute batch insert queries for each category
-    for category_slug, subcategory_batch in batch_data.items():
-        # Check if the category slug exists in the retrieved category IDs
-        if category_slug in category_ids:
-            category_id = category_ids[category_slug]
-        else:
-            print(f"Category slug '{category_slug}' not found in the database.")
-            continue  # Skip this category if the ID is not found
+#     # Prepare and execute batch insert queries for each category
+#     for category_slug, subcategory_batch in batch_data.items():
+#         # Check if the category slug exists in the retrieved category IDs
+#         if category_slug in category_ids:
+#             category_id = category_ids[category_slug]
+#         else:
+#             print(f"Category slug '{category_slug}' not found in the database.")
+#             continue  # Skip this category if the ID is not found
 
-        # Create the base query
-        query = "INSERT INTO subcategories (category_id, subcategory_slug, name, created_at, updated_at) VALUES "
-        query_values = []
+#         # Create the base query
+#         query = "INSERT INTO subcategories (category_id, subcategory_slug, name, created_at, updated_at) VALUES "
+#         query_values = []
 
-        # Temporary secondary seed to subcategories, merge pending
+#         # Temporary secondary seed to subcategories, merge pending
 
-        # Add the subcategory values to the query
-        for subcategory in subcategory_batch:
-            values = f"({category_id}, '{subcategory['subcategory_slug']}', '{subcategory['subcategory_name']}', NOW(), NOW())"
-            query_values.append(values)
+#         # Add the subcategory values to the query
+#         for subcategory in subcategory_batch:
+#             values = f"({category_id}, '{subcategory['subcategory_slug']}', '{subcategory['subcategory_name']}', NOW(), NOW())"
+#             query_values.append(values)
 
-        # Combine the query and execute it
-        query += ", ".join(query_values)
-        query += " ON DUPLICATE KEY UPDATE updated_at = NOW();"
+#         # Combine the query and execute it
+#         query += ", ".join(query_values)
+#         query += " ON DUPLICATE KEY UPDATE updated_at = NOW();"
 
-        # Execute the query using your database method
-        UserSurvey.db.query_db(query)
+#         # Execute the query using your database method
+#         UserSurvey.db.query_db(query)
 
-    return
+#     return
 
 
 def category_seed():
