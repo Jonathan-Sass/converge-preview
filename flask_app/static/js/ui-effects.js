792a5eb6aadf422eb8a3255ff24fc029d879df12
Jonathan <jonsasser@gmail.com>
2025-03-28 10:53:16 -0700

    Update: UI update for more consistent glass layers and styling

--- flask_app/static/js/ui-effects.js
+++ flask_app/static/js/ui-effects.js
@@ -1,6 +1,6 @@
 let lastScrollY = 0;
 let parallaxY = 0;
-let easeFactor = 0.1; // Adjust for more or less smoothing
+let easeFactor = 1; // Adjust for more or less smoothing
 
 function smoothParallax() {
     // Interpolating between last position and target position for smooth effect
